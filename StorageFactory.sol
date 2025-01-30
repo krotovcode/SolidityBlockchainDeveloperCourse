01e6909aba51b68a6485d29819319a56d4c082ae
krotovcode <youngspirits@yandex.ru>
2025-01-30 20:51:56 +0300

    update

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >0.8.19;
+pragma solidity ^0.8.19;
 
-import "./SimpleStorage.sol";
+import {SimpleStorage} from "SimpleStorage.sol";
 
 
 contract StorageFactory {
